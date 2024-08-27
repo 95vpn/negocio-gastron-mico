d4f866adb79eb627a0da760a3909b4ade95e907a
Johnny Herrera <jmjaramillo95@gmail.com>
2024-08-26 19:02:14 -0500

    fotos-editando

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,16 +9,16 @@ const HomePage = () => {
       
       <p>At Homemade Delights, we invite you on a culinary journey full of flavors, aromas, and creativity. From traditional recipes to modern innovations, our platform is designed to inspire chefs of all levels. Whether you are an experienced cook or someone just starting to explore the art of cooking.</p>
       <figure className='home-page-container-figure'>
-        <img src="../public/assets/recipes1.PNG" alt="" />
-        <img src="../public/assets/recipes2.PNG" alt="" />
-        <img src="../public/assets/recipes3.PNG" alt="" />
-        <img src="../public/assets/recipes4.PNG" alt="" />
-        <img src="../public/assets/recipes5.PNG" alt="" />
-        <img src="../public/assets/recipes6.PNG" alt="" />
-        <img src="../public/assets/recipes7.PNG" alt="" />
-        <img src="../public/assets/recipes8.PNG" alt="" />
-        <img src="../public/assets/recipes9.PNG" alt="" />
-        <img src="../public/assets/recipes10.PNG" alt="" />
+        <img src="../assets/recipes1.PNG" alt="" />
+        <img src="../assets/recipes2.PNG" alt="" />
+        <img src="../assets/recipes3.PNG" alt="" />
+        <img src="../assets/recipes4.PNG" alt="" />
+        <img src="../assets/recipes5.PNG" alt="" />
+        <img src="../assets/recipes6.PNG" alt="" />
+        <img src="../assets/recipes7.PNG" alt="" />
+        <img src="../assets/recipes8.PNG" alt="" />
+        <img src="../assets/recipes9.PNG" alt="" />
+        <img src="../assets/recipes10.PNG" alt="" />
       </figure>
       <img src="./" alt="" />
      
