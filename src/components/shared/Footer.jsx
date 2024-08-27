3f39dad1b7874b214514e65053973af642c8af61
Johnny Herrera <jmjaramillo95@gmail.com>
2024-08-26 20:59:22 -0500

    footer-cambios-link

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './styles/footer.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <ul className="footer-container-list">
-        <li className="footer-container-list-item"><a href="#" className="footer-container-list-link">Home</a></li>
-        <li className="footer-container-list-item"><a href="#" className="footer-container-list-link">Products</a></li>
-        <li className="footer-container-list-item"><a href="#" className="footer-container-list-link">Contact</a></li>
-        
+        <li className="footer-container-list-item">
+          <Link to="/" className="footer-container-list-link">Home</Link>
+        </li>
+        <li className="footer-container-list-item">
+          <Link to="/catalogo-de-productos" className="footer-container-list-link">Products</Link>
+        </li>
+        <li className="footer-container-list-item">
+          <Link to="/contacto" className="footer-container-list-link">Contact</Link>
+        </li>
       </ul>
       <p className="text-center text-body-secondary">&copy; 2024 Homemade Delights, Inc</p>
     </footer>
